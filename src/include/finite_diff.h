44205b7c26374dece995a7205963aaa00edd4b27
Edoardo Pasca <edo.paskino@gmail.com>
2019-09-06 12:50:20 +0100

    added include files

---
+++ src/include/finite_diff.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "omp.h"
+#include "dll_export.h"
+
+DLL_EXPORT int fdiff_for(float *inimage, float *outimage, long nx, long ny, int direction);
+DLL_EXPORT int fdiff_for_unrolled(float *inimage, float *outimage, long nx, long ny, int direction);
+DLL_EXPORT int fdiff_for_simd(float *inimage, float *outimage, long nx, long ny, int direction);
+DLL_EXPORT int fdiff_for_parallel(float *inimage, float *outimage, long nx, long ny, int direction);
+DLL_EXPORT int fdiff_parallel_for_simd(float *inimage, float *outimagex, float *outimagey, long nx, long ny);
+DLL_EXPORT int fdiff_parallel_whole(float *inimage, float *outimagex, float *outimagey, long nx, long ny);
+DLL_EXPORT int fdiff_for_parallel_concurrent(float *inimage, float *outimagex, float *outimagey, long nx, long ny);
